74e33175f2a754bb7bd68ba8e5f7d35ee8802eaa
ChrisAtMakeGoodTech <makegoodtech@gmail.com>
2019-05-26 16:01:24 -0500

    default set can't be edited

--- insult-functions.mjs
+++ insult-functions.mjs
@@ -104,7 +104,7 @@ export function activateKey(key) {
         if (keyValue !== null) {
             localStorage.setItem(CONSTANTS.activeKey, key);
             ActiveMap = JSON.parse(keyValue);
-            setUpEdit(ActiveMap);
+            setUpEdit(ActiveMap, key === CONSTANTS.defaultKey);
         }
     }
 }
--- ui-functions.mjs
+++ ui-functions.mjs
@@ -1,12 +1,13 @@
 import { editContainer } from './ui-elements.mjs';
 
-export function setUpEdit(insults) {
+export function setUpEdit(insults, disabled) {
     editContainer.innerHTML = '';
     const maxInsultCount = insults.reduce(getMaxOrCurrentLength, 0);
     for (const insultSet of insults) {
         const insultBox = document.createElement('textarea')
         insultBox.rows = maxInsultCount + 1;
         insultBox.innerHTML = insultSet.join('\n') + '\n';
+        insultBox.disabled = disabled;
         editContainer.appendChild(insultBox);
     }
 }
